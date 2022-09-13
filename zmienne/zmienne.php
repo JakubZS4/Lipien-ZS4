8248de90d8b4873d1e517481f774cba3aadd27d9
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-09-13 09:55:40 +0200

    Create zmienne.php

---
+++ zmienne/zmienne.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne</title>
+</head>
+<body>
+
+    <?php
+        $liczba = 123;
+
+        if($liczba == 0) echo 'Podana liczba jest równa 0';
+        elseif ($liczba > 0)
+            {
+                echo 'Podana liczba jest większa od 0';
+            }
+            else echo 'Podana liczba jest mniejsza od 0';
+    ?>
+
+    
+    
+</body>
+</html>
