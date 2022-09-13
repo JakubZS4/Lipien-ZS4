13e885ed53d3ae2e63fbd755614e361c70760836
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-09-13 09:27:45 +0200

    Create zad1.php

---
+++ zmienne/zad1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zad1</title>
+</head>
+
+<body>
+
+    <?php 
+        echo 'jakis tekst';
+        echo "jakis tekst";
+
+        echo '<h1>nagłówek</h1>';
+        print '<h2>nagłówek</h2>';
+
+        $liczba = 10;
+       // echo $liczba;
+
+        echo '<div>Wartość zmiennej wynosi: '. $liczba . '</div>';
+        echo '//////////////////////////////////////////////////';
+        echo '<div>Wartość zmiennej wynosi:  $liczba </div>';
+        echo "<div>Wartość zmiennej wynosi:  $liczba </div>";
+
+        echo '<div style="color: red; ">tekst</div>';
+    ?>
+    
+</body>
+
+
+</html>
