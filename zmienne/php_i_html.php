9556be9dc5e2c87be717e8d4dbe5b12d8519cab9
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-09-13 09:59:56 +0200

    Create php_i_html.php

---
+++ zmienne/php_i_html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne</title>
+</head>
+<body>
+
+    
+
+    <?php
+        $a = 123;
+        if($a > 0) :
+
+
+    ?>
+
+            <div style="color: blue";>Podana liczba jest większa od zera</div>
+
+            
+            <?php endif; ?>
+
+
+    
+</body>
+</html>
