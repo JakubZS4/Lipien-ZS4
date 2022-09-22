c7b8f00693eb488c91bbec65dc202768f8d0e2c6
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-09-22 21:46:11 +0200

    Create sprawdzanie liczb(nie skończone).php

---
+++ sprawdzanie liczb(nie skończone).php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne</title>
+</head>
+<body>
+
+    
+
+    <?php
+        
+
+        
+       /* $a = -10000; 
+
+        if ($a == 0 && $a > 10 && $a < 100) {
+            echo "Liczba spełnia warunek";
+        } 
+        
+        else {
+            echo "Liczba nie spełnia warunku";
+        }
+        */
+
+        $x = 9;
+if ($x==1 || $x > 10) {
+ echo "Ta instrukcja się wykonała, bo warunek zwraca wartość true.<br>Prawda, że x=1<br>";
+} else {
+ echo "Ta instrukcja się nie wykona, bo warunek zwraca wartość false";
+}
+
+       
+    ?>
+
+
+    
+
+            
+
+            
+            
+
+
+    
+</body>
+</html>
