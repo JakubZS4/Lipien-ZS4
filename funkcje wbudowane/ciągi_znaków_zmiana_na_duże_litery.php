b097696b5469c2775e9ed4ba3fbe834b0e8119cb
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-09-27 10:01:30 +0200

    Create ciągi_znaków_zmiana_na_duże_litery.php

---
+++ funkcje wbudowane/ciągi_znaków_zmiana_na_duże_litery.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ciągi znaków</title>
+</head>
+
+<body>
+    
+    <form action="" method="post">
+        <input type="submit" value="Oblicz">
+    </form>
+
+    <div>
+        <p>Jakis fajny tekst ziom</p>
+    </div>
+
+    
+
+    <?php
+       // $tekst = "To jest zdanie z wielkimi literami";
+       // $konwersja = strtoupper($tekst);
+       // echo $konwersja;
+
+       
+
+
+    ?>
+
+    
+
+
+    
+</body>
+</html>
