b4071feed0614eaac0f56ef76825d1c28721c7ef
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-10-18 09:23:12 +0200

    Add files via upload

---
+++ sesje/sesje/strona.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+
+    //zabezpieczenie
+    if(!isset($_SESSION['czy_zalogowany']) || $_SESSION
+        ['czy_zalogowany'] == false)
+        {
+            header('Location: loguj.php');
+            exit;
+        }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesje</title>
+</head>
+<body>
+
+    <h1>Witaj w serwisie promującym język PHP.</h1>
+    <h3>Jesteś zalogowany jako: <?php echo $_SESSION['login_uzytkownika']; ?>
+    </h3>
+    <a href="wyloguj.php">Wyloguj się</a>
+
+</body>
+</html>
