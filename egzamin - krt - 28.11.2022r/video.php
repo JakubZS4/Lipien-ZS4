d089959e997d4c48bc579a9bbaee40833dde12da
JakubZS4 <90259443+JakubZS4@users.noreply.github.com>
2022-11-28 08:45:22 +0100

    Add files via upload

---
+++ egzamin - krt - 28.11.2022r/video.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video on Demand</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+
+<body>
+
+    <header class="baner-lewy">
+        <h1>Internetowa wypożyczalnia filmów</h1>
+    </header>
+    <main>
+        
+        
+        <section class="baner-prawy">
+        <table>
+        <tr>
+             <td>Kryminał</td> <td>Horror</td> <td>Przygodowy</td>
+         </tr>
+        <tr>
+            <td>20</td> <td>30</td> <td>20</td>
+         </tr>
+        </table>
+        </section>
+
+        <list class="Polecamy">
+            <h3>Polecamy</h3>
+            <div>
+                <p>18. Alien 4</p>
+                <img src="alien.jpg"/>
+            </div>
+
+            <div>
+                <p>22. Koralowa wyspa</p>
+                <img src="koralowa_wyspa.jpg"/>
+            </div>
+
+            <div>
+                <p>23. Krokodyl</p>
+                <img src="krokodyl.jpg"/>
+            </div>
+
+            <div>
+                <p>25. Steam punk</p>
+                <img src="steampunk.jpg"/>
+            </div>
+        </list>
+        
+        <list class="Filmyfantastyczne">
+        <h3>Polecamy</h3>
+            <div>
+                <p>9. Star Wars</p>
+                <img src="star-wars.jpg"/>
+            </div>
+
+            <div>
+                <p>11. E.T</p>
+                <img src="koralowa_wyspa.jpg"/>
+            </div>
+
+            <div>
+                <p>13. Hobbit2</p>
+                <img src="hobbit.jpg"/>
+            </div>
+
+            <div>
+                <p>14. Fantasy2</p>
+                <img src="fantasy.jpg"/>
+            </div>
+
+        </list>
+
+        
+    </main>
+    <footer class="stopka">
+        
+    </footer>
+</body>
+</html>
